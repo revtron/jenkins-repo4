e4a032dc8d6ba647de2853343cf2e715533a200f
Sohail Ahmed B <75356456+revtron@users.noreply.github.com>
2025-04-29 14:28:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "newbucket" { 
+  count = 2
+}
+
+resource "aws_instance" "new-instance" { 
+    instance_type = "t2-micro"
+    key_name = "jenkins_key"
+    ami = "ami-084568db4383264d4"
+      tags = { 
+       Name = "EC2-Terraform-Jenkins"
+
+    }
+}
